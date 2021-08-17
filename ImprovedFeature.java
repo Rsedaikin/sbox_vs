5cbcaf83f614c7fd1c9882dfea76dfe22a97a62c
VSedaikina <vsedaikina@gmail.com>
2021-08-17 13:38:19 +0300

    Improve improved feature.

--- ImprovedFeature.java
+++
@@ -1,5 +0,0 @@
-public class ImprovedFeature {
-    public static String concat(String s, int i) {
-        return (s + " " + i);
-    }
-}
--- feature.java
+++ feature.java
@@ -3,4 +3,8 @@ public class Feature {
     static int calculation() {
         return 42;
     }
+
+    public static String concat(String s) {
+        return (s + " " + calculation());
+    }
 }
