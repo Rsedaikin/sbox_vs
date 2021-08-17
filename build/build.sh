1bb79120cc69448550475b81296e8c6206a40027
VSedaikina <vsedaikina@gmail.com>
2021-08-17 12:29:21 +0300

    Added build system for [file.java].

---
+++ build/build.sh
@@ -0,0 +1,3 @@
+rm file.java
+cp ../file.java file.java
+java file.java
---
+++ file.java
@@ -0,0 +1,7 @@
+package sbox;
+class file {
+    public static void main(String... args) {
+        System.out.println("hello world!");
+    }
+
+}
